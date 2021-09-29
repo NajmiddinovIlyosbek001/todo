e9a832dc250d7e17cb8ca1009fb4c92cec3e7869
NajmiddinovIlyosbek001 <89776281+NajmiddinovIlyosbek001@users.noreply.github.com>
2021-09-29 21:57:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 function App() {
   return (
     <div id="outer-container">
+      <div className="nav">
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <p>User</p>
+        </ul>
+      </div>
       <div id="sidebar">
         <p>This is sidebar</p>
       </div>
